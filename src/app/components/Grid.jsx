1f87436b2d41fe122f13eaa66e30873944fb9913
legodev <gonzalezleonardo283@gmail.com>
2024-11-26 21:10:11 -0300

    Added home images

--- src/app/components/Grid.jsx
+++ src/app/components/Grid.jsx
@@ -4,49 +4,50 @@ export default function Grid() {
   return (
     <section className="home__grid">
       <figure className="home__figure grid-1">
-        <Image
+      <Image
           className="image"
-          src="/image/hi.jpeg"
+          src="/image/img-1.jpg"
           alt="image"
-          height={100}
-          width={100}
+          height={1080}
+          width={1920}
         />
       </figure>
       <figure className="home__figure grid-2">
         <Image
           className="image"
-          src="/image/hi.jpeg"
+          src="/image/img-2.jpg"
           alt="image"
-          height={100}
-          width={100}
+          height={800}
+          width={550}
         />
       </figure>
       <figure className="home__figure grid-3">
         <Image
           className="image"
-          src="/image/hi.jpeg"
+          src="/image/img-3.jpg"
           alt="image"
-          height={100}
-          width={100}
+          height={972}
+          width={1620}
         />
       </figure>
       <figure className="home__figure grid-4">
         <Image
           className="image"
-          src="/image/hi.jpeg"
+          src="/image/img-4.jpg"
           alt="image"
-          height={100}
-          width={100}
+          height={618}
+          width={1050}
         />
       </figure>
 
       <figure className="home__figure grid-5">
-        <Image
+
+          <Image
           className="image"
-          src="/image/hi.jpeg"
+          src="/image/img-5.jpg"
           alt="image"
-          height={100}
-          width={100}
+          height={972}
+          width={1620}
         />
       </figure>
     </section>
